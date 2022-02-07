079d66458423024e6e228be8240d45546efda4f4
BolderCoder8 <92707070+BolderCoder8@users.noreply.github.com>
2022-02-07 16:19:54 +1100

    Styled application

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <h1>Dictionary</h1>
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
           <small>Coded by Laura Mooney</small>
